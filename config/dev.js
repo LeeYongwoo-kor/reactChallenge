0b9ed9dc1a974d32c12cb9c84f54665daaa0b8bf
LeeYongwoo-kor <newlyo1205@naver.com>
2021-02-03 23:52:37 +0900

    second commit

---
+++ config/dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://Lee-Yongwoo:abcd1234@sunplate.e2ghh.mongodb.net/sunplate?retryWrites=true&w=majority",
+};
--- index.js
+++ index.js
@@ -1,6 +1,14 @@
+const bodyParser = require("body-parser");
 const express = require("express");
 const app = express();
 const port = 5000;
+const { User } = require("./models/User.js");
+
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// application/json
+app.use(bodyParser.json());
 
 const mongoose = require("mongoose");
 mongoose
@@ -18,6 +26,18 @@ mongoose
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.post("/register", (req, res) => {
+  // 회원가입할때 필요한 정보들을 client에서 가져오면
+  // 그것들을 데이터베이스에 넣어준다.
+  const user = new User(req.body);
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
 });
 
 app.listen(port, () => {
